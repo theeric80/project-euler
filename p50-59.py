2d8229b0a9ef179ea932916944aad1310e39c82e
Eric Tsai <goose99_tw@yahoo.com.tw>
2013-08-01 08:56:15 +0800

    [what] problem52

--- p50-59.py
+++ p50-59.py
@@ -1,4 +1,5 @@
-from emath import prime_sieve
+from itertools import count, dropwhile
+from emath import prime_sieve, is_permuted
 
 # Consecutive prime sum
 def problem50():
@@ -19,6 +20,17 @@ def problem50():
     ret = max(ret, key=lambda x: len(x[1]))[0]
     assert(ret == 997651)
     print 'problem50 = %d' % ret
+
+# Permuted multiples
+def problem52():
+    def match(a):
+        return all(is_permuted(a[0], a[i]) for i in xrange(1, len(a)))
+
+    ret = dropwhile(lambda x: not match(x), ([n*i for i in xrange(1, 7)] for n in count(1))).next()
+    ret = ret[0]
+
+    assert(ret == 142857)
+    print 'problem52 = %d' % ret
 
 if __name__ == '__main__':
     for i in xrange(50, 60):
