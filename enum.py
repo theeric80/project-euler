49d72cfe5ee82d5f6d1d47fc3decf49b6d9dabdb
Eric Tsai <eric_tsai@cyberlink.com>
2013-08-07 07:05:58 +0800

    [what] add function: square, heptagonal, octagonal

--- enum.py
+++ enum.py
@@ -9,11 +9,20 @@ def fibonacci_n(x):
 def triangle_n(x):
     return x * (x + 1) / 2
 
+def square_n(x):
+    return x * x
+
 def pentagonal_n(x):
     return x * (3 * x - 1) / 2
 
 def hexagonal_n(x):
     return x * (2 * x - 1)
+
+def heptagonal_n(x):
+    return x * (5 * x - 3) / 2
+
+def octagonal_n(x):
+    return x * (3 * x - 2)
 
 def fibonacci(start=1):
     a, b = fibonacci_n(start), fibonacci_n(start + 1)
@@ -32,6 +41,10 @@ def triangle(start=1):
     for i in count(start):
         yield triangle_n(i)
 
+def square(start=1):
+    for i in count(start):
+        yield square_n(i)
+
 def pentagonal(start=1):
     for i in count(start):
         yield pentagonal_n(i)
@@ -39,6 +52,14 @@ def pentagonal(start=1):
 def hexagonal(start=1):
     for i in count(start):
         yield hexagonal_n(i)
+
+def heptagonal(start=1):
+    for i in count(start):
+        yield heptagonal_n(i)
+
+def octagonal(start=1):
+    for i in count(start):
+        yield octagonal_n(i)
 
 if __name__ == '__main__':
     print 'Unittest: %s' % __file__
