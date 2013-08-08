e6ff0f55700c1db79fbef200e482f2febbfc5b08
Eric Tsai <eric_tsai@cyberlink.com>
2013-08-08 21:45:45 +0800

    [what] refine function: prime_factor

--- emath.py
+++ emath.py
@@ -1,7 +1,7 @@
 import math
 from random import randint
 import operator
-from itertools import count, imap
+from itertools import count, chain, imap
 
 def profile(func):
     def inner(*args, **kwargs):
@@ -138,14 +138,15 @@ def prime_sieve_s(x):
     return ret
 
 def prime_factor(x):
+    # Trial Division
     ret = {}
-    i = 2
-    while x > 1:
-        if x % i == 0:
+    sqrt_x = int(x**0.5)
+    for i in chain([2], xrange(3, sqrt_x + 1, 2)):
+        while x % i == 0:
             x /= i
             ret[i] = ret.get(i, 0) + 1
-        else:
-            i += (2 if i > 2 else 1)
+    if x > 1:
+        ret[x] = ret.get(x, 0) + 1
     return ret
 
 def proper_divisor(x):
