d95bc9bb7f3c46ae68ad7a92b30eb6be90925158
Eric Tsai <eric_tsai@cyberlink.com>
2013-08-10 00:10:56 +0800

    [what] problem71

--- p70-79.py
+++ p70-79.py
@@ -1,5 +1,5 @@
-from itertools import islice
-from emath import profile, prime_sieve, phi, is_permuted
+from itertools import islice, dropwhile
+from emath import profile, prime_sieve, prime_factor_p, phi, is_permuted
 
 # Totient permutation
 def problem70():
@@ -24,6 +24,30 @@ def problem70():
 
     assert(ret == 8319823)
     print 'problem70 = %d' % ret
+
+# Ordered fractions
+def problem71():
+    x = 1000000
+    D = {1: set()}
+    f37 = 3.0 / 7
+    ret = [0, (0, 1)]
+    primes = prime_sieve(int(x**0.5) + 1)
+
+    for d in xrange(2, x + 1):
+        D[d] = set(prime_factor_p(d, primes).keys())
+
+        # n/d: the first reduced proper fraction for d to the left of 3/7
+        # n/d < 3/7 ==> b = int(3.0 / 7 * d)
+        b = max(int(f37 * d), 1)
+        n = dropwhile(lambda n: D[n].intersection(D[d]), (n for n in xrange(b, 0, -1))).next()
+
+        f = float(n) / d
+        if f < f37 and f > ret[0]:
+            ret = [f, (n, d)]
+
+    ret = ret[1][0]
+    assert(ret == 428570)
+    print 'problem71 = %d' % ret
 
 if __name__ == '__main__':
     for i in xrange(70, 80):
